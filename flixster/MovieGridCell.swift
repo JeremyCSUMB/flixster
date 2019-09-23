629d42053dfe8da89f77f996f1d913a4968b0edf
Jeremy Shaw <jjsjeremy61@>
2019-09-23 14:27:34 -0700

    Finished part 1

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Jeremy Shaw on 9/23/19.
+//  Copyright © 2019 Jeremy Shaw. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
